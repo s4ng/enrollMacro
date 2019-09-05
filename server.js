643b986a2cfb4b0abcb66e42547b67af01609916
s4ng <zalcls951201@gmail.com>
2019-09-05 23:53:56 +0900

    first uploada

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
 let macro = require('./start');
+const PORT = process.env.PORT;
 
 app.use(express.urlencoded());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000, function(){
+var server = app.listen(PORT, function(){
     console.log("Express server has started on port 3000")
 });
 app.use(express.static('public'));
